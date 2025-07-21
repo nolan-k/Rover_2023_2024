1de1042c2c5b84b9bc110a65affcfd49d0aab37b
Jnorth2 <jar3dnorth51@gmail.com>
2025-07-21 10:29:39 -0700

    Added documentation

--- software/ros_packages/rover2_control/rover2_control/gripper_control.py
+++ software/ros_packages/rover2_control/rover2_control/gripper_control.py
@@ -62,20 +62,21 @@ class GripperCanControl(Node):
         self.setup_controller()
 
     def timer_callback(self):
+        """This function sends can commands to the odrive at consistent frequency (required for velocity mode). 
+        It also handles no controller input and ensuring the motor reaches desired set points. 
+        
+        """
         #complete Homing
         if not self.is_homed:
-            self.get_logger().info("homing")
             self.set_mode(2)
             self.send_velocity(self.home_vel)
         #timeout period of half sec
         elif abs(self.last_message_time - time()) > 0.5 and self.mode != 1:
             #self.get_logger().info("No inputs")
             if self.mode != 3:
-                self.get_logger().info("here")
                 self.set_mode(3)
                 self.send_position(pos=self.current_pos)
             elif abs(self.current_pos - self.pos_setpoint) > 0.02 and self.current_vel < 0.01:
-                self.get_logger().info("out of position")
                 self.send_position(self.pos_setpoint)
         #handle going to torque mode
         elif self.mode == 1:
@@ -93,39 +94,49 @@ class GripperCanControl(Node):
                 self.send_position(self.pos_setpoint)
 
     def joy_callback(self, msg):
-
+        """Handles controller inputs. 
+        """
         self.last_message_time = time()
         buttons = msg.buttons
+        #wait for homing to complete
         if self.is_homed:
             if buttons[self.open_button]: 
+                #ensure we don't go past fully open position
                 if self.current_pos >= self.home_pos:
                     self.set_mode(3)
                     self.pos_setpoint = self.home_pos
+                #set velocity mode and setpoint
                 else:
                     self.set_mode(2)
                     self.vel = self.vel_setpoint
 
             elif buttons[self.close_button]: 
+                #Check if grasping an object or if the gripper is closed and Hold Torque
                 if self.current > self.current_threshold:
                     self.set_mode(1)
                 else: 
                     self.set_mode(2)
                     self.vel = -self.vel_setpoint
-            else:
+            # Stop moving if not in torque control
+            elif self.mode != 1:
                 self.set_mode(3)
                 self.pos_setpoint = self.current_pos
 
     def setup_controller(self):
+        """Initialize controller.
+        """
+        #initialize control mode
         self.set_mode(self.mode)
         self.home()
         self.get_logger().info("Finished Setup")
 
     def home(self):
+        """Find home position by hitting hardstop limit.
+        """
         self.get_logger().info("Start Homing Sequence")
         self.set_mode(2) #enable closed loop ramped velocity mode
         while True:
             rclpy.spin_once(self, timeout_sec=0.01)
-            #self.get_logger().info("In while loop")
             if self.current > self.home_current_threshold:
                 self.home_pos = self.current_pos + self.home_offset
                 self.is_homed = True
@@ -137,28 +148,63 @@ class GripperCanControl(Node):
         self.last_message_time = time()
 
     def send_torque(self, torq):
+        """Send a torque set point through can.
+        
+        Parameters
+        ----------
+            torq : float
+                A torque set point. 
+        """
         self.bus.send(can.Message(
             arbitration_id=(self.node_id << 5 | 0x0e),
             data=struct.pack('<f', torq)
         ))
 
-    def send_velocity(self, vel):
+    def send_velocity(self, vel, torq_ff = 0.0):
+        """Send a velocity set point through can.
+        
+        Parameters
+        ----------
+            vel : float
+                A velocity setpoint.
+            torq_ff : float, optional
+                The torque feed forward value.
+        """
         self.bus.send(can.Message(
             arbitration_id=(self.node_id << 5 | 0x0d), # 0x0d: Set_Input_Vel
-            data=struct.pack('<ff', vel, 0.0), # 1.0: velocity, 0.0: torque feedforward
+            data=struct.pack('<ff', vel, torq_ff), # 1.0: velocity, 0.0: torque feedforward
             is_extended_id=False
         ))
         #self.get_logger().info(f"Sending Velocity: {vel}")  
         
-    def send_position(self, pos):
+    def send_position(self, pos, vel_ff = 0, torq_ff = 0):
+        """Send a position set point through can.
+        
+        Parameters
+        ----------
+            pos : float
+                A position set point. 
+            vel_ff : int, optional
+                The velocity feed forward value in 1/1000 rev/s.
+            torq_ff : int, optional
+                The torque feed forward value in 1/1000 Nm/s.
+        """
         self.bus.send(can.Message(
             arbitration_id = (self.node_id << 5 | 0x0c),
-            data = struct.pack('<fHH', pos, 0, 0),
+            data = struct.pack('<fHH', pos, vel_ff, torq_ff),
             is_extended_id = False
         ))
         self.pos_setpoint = pos
 
     def set_mode(self, mode):
+        """Sets the desired control mode. 
+        
+        Parameters
+        ----------
+            mode : {0, 1, 2, 3}
+                The desired control mode {idle, closed loop torque control, closed loop ramped velocity 
+                control, closed loop trapezoidal trajectory control}
+        """
         if self.mode != mode:
             self.get_logger().info(f"mode: {mode}")
             match mode:
@@ -237,6 +283,13 @@ class GripperCanControl(Node):
 
     #Define a callback for watching can messages:
     def read_can(self):
+        """Processes incoming can messages.
+
+        This function reads a buffer of can messages sent on the bus by odrives and pulls out important 
+        command values. Two commands are searched for: Encoder Estimate and Motor Current (get_IQ). 
+        Encoder estimate contains the measured position and velocity while motor current contains the 
+        measured motor current and setpoint. 
+        """
         #Get a buffer of stored messages and iterate over it
         can_msgs = self.get_can_buffer()
 
@@ -265,7 +318,7 @@ class GripperCanControl(Node):
                         pass	
                     case 0x09: #Encoder Estimate of Position/Velocity
                         pos_estimate, vel_estimate = struct.unpack('<ff', bytes(can_msg.data))
-                        self.get_logger().info(f'position: {pos_estimate}')
+                        # self.get_logger().info(f'position: {pos_estimate}')
                         self.current_pos = pos_estimate
                         self.current_vel = vel_estimate
                     
@@ -273,12 +326,22 @@ class GripperCanControl(Node):
                         iq_set, iq_measured = struct.unpack('<ff', bytes(can_msg.data))
                         self.current = iq_measured
 
-                        self.get_logger().info(f"Current: {iq_measured}, Set: {iq_set}")
+                        # self.get_logger().info(f"Current: {iq_measured}, Set: {iq_set}")
 
                     #case 0x1c: #Torque Target/Estimate
 
     #Abstraction for getting all can messages currently in the buffer:
     def get_can_buffer(self):
+        """Stores can messages in a buffer.'
+
+        This function reads and stores can messages to a buffer for asynchronus parsing. Stores a maximum of
+        1000 messages at a time. 
+
+        Returns
+        -------
+            can_msgs : list
+                The buffer of can messages. 
+        """
 
         #Return a max of 1000 msgs, that way we don't miss publishing
         max_return_msgs = 1000
@@ -306,25 +369,6 @@ class GripperCanControl(Node):
 
         #Return list of msgs
         return can_msgs
-
-    def destroy_node(self):
-        # Set axis to IDLE before shutdown
-        self.get_logger().info("Shutting down... setting ODrive to IDLE")
-
-        try:
-            self.bus.send(can.Message(
-                arbitration_id=(self.node_id << 5 | 0x07),  # Set_Axis_State
-                data=struct.pack('<I', 1),  # 1 = AXIS_STATE_IDLE
-                is_extended_id=False
-            ))
-        except Exception as e:
-            self.get_logger().error(f"Failed to send IDLE to ODrive: {e}")
-
-        # Call the parent class's destroy_node to clean up timers/subs
-        super().destroy_node()
-
-
-
 
 def main(args=None):
     rclpy.init(args=args)
