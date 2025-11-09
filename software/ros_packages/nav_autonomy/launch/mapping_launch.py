6be3e134e7ffd41ab157880764efe1e6e884ae63
Kylan <kyrileyj@gmail.com>
2025-11-09 14:49:18 -0800

    add rtab to dependencies

--- software/ros_packages/nav_autonomy/launch/mapping_launch.py
+++ software/ros_packages/nav_autonomy/launch/mapping_launch.py
@@ -4,8 +4,6 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node, SetParameter
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition
